5ed390847d211ae9959248e68b61768c591cb3e4
HangNT169 <HangNT169@fpt.edu.vn>
2023-03-19 00:40:11 +0700

    Update

---
+++ src/main/java/com/poly/hangnt169/controller/SanPhamServlet.java
@@ -0,0 +1,23 @@
+package com.poly.hangnt169.controller;
+/**
+ * @author hangnt169
+ */
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "SanPhamServlet", value = "/SanPhamServlet")
+public class SanPhamServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
--- src/main/java/com/poly/hangnt169/util/HibernateUtil.java
+++ src/main/java/com/poly/hangnt169/util/HibernateUtil.java
@@ -8,6 +8,9 @@ import org.hibernate.service.ServiceRegistry;
 
 import java.util.Properties;
 
+/**
+ * @author hangnt169
+ */
 public class HibernateUtil {
 
     private static final SessionFactory FACTORY;
@@ -16,11 +19,11 @@ public class HibernateUtil {
         Configuration conf = new Configuration();
 
         Properties properties = new Properties();
-        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-        properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/SOF3011_CRUD");
-        properties.put(Environment.USER, "root");
-        properties.put(Environment.PASS, "root");
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.SQLServerDialect");
+        properties.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        properties.put(Environment.URL, "jdbc:sqlserver://localhost:1433;databaseName=SOF3011_CRUD");
+        properties.put(Environment.USER, "sa");
+        properties.put(Environment.PASS, "Password.1");
         properties.put(Environment.SHOW_SQL, "true");
 
         conf.setProperties(properties);
@@ -33,5 +36,9 @@ public class HibernateUtil {
 
     public static SessionFactory getFACTORY() {
         return FACTORY;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getFACTORY());
     }
 }
