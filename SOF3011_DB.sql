7237fda3570561afd94b5ef52cbf55d1c4233a7a
HangNT169 <104345338+HangNT169@users.noreply.github.com>
2023-03-19 00:32:20 +0700

    Update SOF3011_DB.sql

--- SOF3011_DB.sql
+++ SOF3011_DB.sql
@@ -1,32 +1,29 @@
 CREATE DATABASE SOF3011_CRUD;
-
+GO
 USE SOF3011_CRUD;
-
-CREATE TABLE SOF3011_CRUD.san_pham (
-	id BIGINT auto_increment NOT NULL,
-	ten_san_pham varchar(100) NULL,
-	loai varchar(100) NULL,
+go
+CREATE TABLE san_pham (
+	id BIGINT IDENTITY NOT NULL,
+	ten_san_pham nvarchar(100) NULL,
+	loai nvarchar(100) NULL,
 	so_luong_ton INTEGER NULL,
-	gia DOUBLE NULL,
-	mieu_ta varchar(100) NULL,
+	gia float null ,
+	mieu_ta nvarchar(100) NULL,
 	CONSTRAINT san_pham_PK PRIMARY KEY (id)
 )
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_0900_ai_ci;
-
-INSERT INTO SOF3011_CRUD.san_pham
+SET IDENTITY_INSERT san_pham ON
+INSERT INTO san_pham
 (id, ten_san_pham, loai, so_luong_ton, gia, mieu_ta)
-VALUES(1, 'Áo len', 'Áo', 100, 15000.0, 'Hàng đẹp');
-INSERT INTO SOF3011_CRUD.san_pham
+VALUES(1, N'Áo len', 'Áo', 100, 15000.0, N'Hàng đẹp');
+INSERT INTO san_pham
 (id, ten_san_pham, loai, so_luong_ton, gia, mieu_ta)
-VALUES(3, 'Quần dài ', 'Quần', 1000, 300000.0, 'Phù hợp với những bạn cao');
-INSERT INTO SOF3011_CRUD.san_pham
+VALUES(3, N'Quần dài ', N'Quần', 1000, 300000.0, N'Phù hợp với những bạn cao');
+INSERT INTO san_pham
 (id, ten_san_pham, loai, so_luong_ton, gia, mieu_ta)
-VALUES(4, 'Quần sooc', 'Quần ', 200, 50000.0, 'Phù hợp thời tiết mát mẻ');
-INSERT INTO SOF3011_CRUD.san_pham
+VALUES(4, N'Quần sooc', N'Quần ', 200, 50000.0, N'Phù hợp thời tiết mát mẻ');
+INSERT INTO san_pham
 (id, ten_san_pham, loai, so_luong_ton, gia, mieu_ta)
-VALUES(5, 'Váy cưới', 'Váy', 100, 100000000, 'Phù hợp ');
-INSERT INTO SOF3011_CRUD.san_pham
+VALUES(5, N'Váy cưới', N'Váy', 100, 100000000, N'Phù hợp ');
+INSERT INTO san_pham
 (id, ten_san_pham, loai, so_luong_ton, gia, mieu_ta)
 VALUES(2, 'Áo khoác ', 'Áo', 250, 250000.0, 'Áo khoác quảng châu');
