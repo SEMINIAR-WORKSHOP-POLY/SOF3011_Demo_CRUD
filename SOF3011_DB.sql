7003187efac6f990b6e305b231632e5e9a58179e
HangNT169 <104345338+HangNT169@users.noreply.github.com>
2023-03-24 15:27:31 +0700

    Update SOF3011_DB.sql

--- SOF3011_DB.sql
+++ SOF3011_DB.sql
@@ -11,19 +11,18 @@ CREATE TABLE san_pham (
 	mieu_ta nvarchar(100) NULL,
 	CONSTRAINT san_pham_PK PRIMARY KEY (id)
 )
-SET IDENTITY_INSERT san_pham ON
 INSERT INTO san_pham
-(id, ten_san_pham, loai, so_luong_ton, gia, mieu_ta)
-VALUES(1, N'Áo len', 'Áo', 100, 15000.0, N'Hàng đẹp');
+(ten_san_pham, loai, so_luong_ton, gia, mieu_ta)
+VALUES( N'Áo len', 'Áo', 100, 15000.0, N'Hàng đẹp');
 INSERT INTO san_pham
-(id, ten_san_pham, loai, so_luong_ton, gia, mieu_ta)
-VALUES(3, N'Quần dài ', N'Quần', 1000, 300000.0, N'Phù hợp với những bạn cao');
+( ten_san_pham, loai, so_luong_ton, gia, mieu_ta)
+VALUES(N'Quần dài ', N'Quần', 1000, 300000.0, N'Phù hợp với những bạn cao');
 INSERT INTO san_pham
-(id, ten_san_pham, loai, so_luong_ton, gia, mieu_ta)
-VALUES(4, N'Quần sooc', N'Quần ', 200, 50000.0, N'Phù hợp thời tiết mát mẻ');
+( ten_san_pham, loai, so_luong_ton, gia, mieu_ta)
+VALUES( N'Quần sooc', N'Quần ', 200, 50000.0, N'Phù hợp thời tiết mát mẻ');
 INSERT INTO san_pham
-(id, ten_san_pham, loai, so_luong_ton, gia, mieu_ta)
-VALUES(5, N'Váy cưới', N'Váy', 100, 100000000, N'Phù hợp ');
+( ten_san_pham, loai, so_luong_ton, gia, mieu_ta)
+VALUES( N'Váy cưới', N'Váy', 100, 100000000, N'Phù hợp ');
 INSERT INTO san_pham
-(id, ten_san_pham, loai, so_luong_ton, gia, mieu_ta)
-VALUES(2, 'Áo khoác ', 'Áo', 250, 250000.0, 'Áo khoác quảng châu');
+( ten_san_pham, loai, so_luong_ton, gia, mieu_ta)
+VALUES( 'Áo khoác ', 'Áo', 250, 250000.0, 'Áo khoác quảng châu');
